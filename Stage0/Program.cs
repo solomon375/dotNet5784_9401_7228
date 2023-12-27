df915490708d48fcf68181442e4200bc5ca8c345
shlom <shlom@Salhoov@g.jct.ac.il>
2023-12-27 16:19:45 +0200

    stage0-init

---
+++ Stage0/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Stage0;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hell o, World!");
+    }
+}
