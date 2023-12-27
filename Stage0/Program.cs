57e918ad99286d3d6bcfd6f46c55bf18dd150a53
shlom <shlom@Salhoov@g.jct.ac.il>
2023-12-27 16:48:03 +0200

    Initial Code+Welcome functions

--- Stage0/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-namespace Stage0;
- class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hell o, World!");
-    }
-}
---
+++ Stage0/Program7228.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Stage0;
+ partial class Program
+{
+     static void Main(string[] args)
+    {
+        Welcome7228();
+        Welcome9401();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome9401();
+
+    private static void Welcome7228()
+    {
+        Console.WriteLine("Enter your name: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", username);
+    }
+}
