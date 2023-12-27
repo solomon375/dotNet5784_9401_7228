dbd6a8e955516f77d71706a0f22e3dedc94b15cc
matanya.nabet <mnabet@g.jct.ac.il>
2023-12-27 18:14:47 +0200

    Stage0 - 11

---
+++ Stage0/Program9401.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Stage0;
+partial class Program
+{
+
+    static partial void Welcome9401()
+    {
+        Console.WriteLine("Day taazov oti ");
+    }
+}
