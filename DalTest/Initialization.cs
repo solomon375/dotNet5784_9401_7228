7ea85523d07b756943dc8a22cf5fa366f84bf708
matanya.nabet <mnabet@g.jct.ac.il>
2024-01-07 20:28:08 +0200

    stage 8 b complete

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,12 @@
+﻿namespace DalTest;
+
+using DalApi;
+using DO;
+public static class Initialization
+{
+    private static ITask? s_delTask;
+    private static IEngineer? s_delEngineer;
+    private static IDependency? s_delDependency;
+
+    private static readonly Random s_rand = new();
+}
