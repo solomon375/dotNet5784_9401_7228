2000aea61eb08f481a44c1f6d0895d134e7514ba
matanya.nabet <mnabet@g.jct.ac.il>
2024-02-19 00:58:06 +0200

    in app.xaml line 9 in EngineerListWindow.xaml line 26

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -10,7 +10,7 @@ namespace PL
     internal class EngineerCollection : IEnumerable
     {
         static readonly IEnumerable<BO.EngineerExperience> s_enums =
-    (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!;
+            (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!;
 
         public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
     }
